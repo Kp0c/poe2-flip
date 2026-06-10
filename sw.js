a6ab618dcf102c4e98fd01d720ef0f5d234011ac
Andrii Yushchenko <andrii.yuschenko@nethealth.com>
2026-06-10 10:27:22 +0300

    PoE2 Flip PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'poe2-flip-v2';
+const STATIC_ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const isHtmlPage = event.request.mode === 'navigate'
+    || event.request.url.endsWith('/index.html');
+
+  if (isHtmlPage) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
